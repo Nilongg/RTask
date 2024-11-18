1d0c1392d560eceac77351f25d1f5012222d938f
Nilongg <99095762+Nilongg@users.noreply.github.com>
2024-11-18 11:11:48 +0200

    Home.jsx: initialize dataFetching logic

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,25 @@
 // FIle for home page functionality
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = () => {
+    useEffect(() => {
+        loadData().then((data) => {
+            console.log(data);
+        });
+    }, []);
+
     return (
         <div>
             <h1>Home Page</h1>
         </div>
     );
 }
+const loadData = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Data loaded');
+        }, 2000);
+    });
+};
 
 export default Home;
